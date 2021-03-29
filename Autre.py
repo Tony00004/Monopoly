8f2bba9c07c462208a828eb76d5a4066e7611dc7
Tony <anto.95.cas@hotmail.com>
2021-03-29 17:09:45 -0400

    Ça avance bien !

--- Autre.py
+++ Autre.py
@@ -1,10 +1,10 @@
 class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+	def __init__(self, name, age):
+		self.name = name
+		self.age = age
 
-  def myfunc(self):
-    print("Hello my name is " + self.name)
+	def myfunc(self):
+		print("Hello my name is " + self.name)
 
 p1 = Person("John", 36)
 p1.myfunc()
